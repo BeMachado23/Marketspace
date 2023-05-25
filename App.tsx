a46c93210a3e90b91ae5221a1236f4c5e5504f4a
Venom Infinity <99411987+BeMachado23@users.noreply.github.com>
2023-05-25 19:22:36 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,16 +1,35 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function App() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   return (
     <View style={styles.container}>
-      <Image source = {(require('./assets/logo.png'))} style={{width: 100, height:100, resizeMode: 'contain', marginTop:60}}/>
-      <Text style = {styles.titulo}>marketspace</Text>
-      <Text style = {styles.subtitulo}>Seu espaço de compra e venda</Text>
-      <Text style = {styles.comando}>Acesse sua conta</Text>
+      <Image source={(require('./assets/logo.png'))} style={{ width: 100, height: 100, resizeMode: 'contain', marginTop: 60 }} />
+      <Text style={styles.titulo}>marketspace</Text>
+      <Text style={styles.subtitulo}>Seu espaço de compra e venda</Text>
+      <Text style={styles.comando}>Acesse sua conta</Text>
       <View>
-      <TextInput placeholder='  E-mail' style={styles.input} placeholderTextColor = '#cccace'></TextInput>
-      <TextInput placeholder='  Senha' style={styles.input} placeholderTextColor = '#cccace'></TextInput>
+        <TextInput placeholder='  E-mail' style={styles.input} placeholderTextColor='#cccace' />
+        <View style={styles.passwordInputContainer}>
+          <TextInput
+            placeholder='  Senha'
+            style={styles.passwordInput}
+            secureTextEntry={!passwordVisible}
+            placeholderTextColor='#cccace'
+          />
+          <TouchableOpacity style={styles.eyeIconContainer} onPress={togglePasswordVisibility}>
+            <Ionicons name={passwordVisible ? 'eye-off' : 'eye'} size={24} color='#888' />
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity style={styles.join}>Entrar</TouchableOpacity>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -20,28 +39,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#EDECEE',
     alignItems: 'center',
   },
-  titulo:{
+  titulo: {
     fontSize: 34,
     fontWeight: 'bold'
   },
-  subtitulo:{
+  subtitulo: {
     fontSize: 14,
     color: '#89871B',
   },
-  comando:{
+  comando: {
     marginTop: 90,
-    fontWeight: 500,
+    fontWeight: '500',
     color: '#89871B',
     fontSize: 12.5,
   },
-  input:{
+  input: {
     width: 250,
     height: 50,
-    backgroundColor:'#f7f7f8',
+    backgroundColor: '#f7f7f8',
     marginTop: 10,
     borderRadius: 8
-  }
+  },
+  passwordInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  passwordInput: {
+    flex: 1,
+    height: 50,
+    backgroundColor: '#f7f7f8',
+    borderRadius: 8,
+    paddingRight: 40, // Espaço para o ícone de olho
+  },
+  eyeIconContainer: {
+    position: 'absolute',
+    right: 10,
+    top: 10,
+  },
+  join: {
+    textAlign: 'center',
+    padding: 10,
+    backgroundColor: '#647AC6',
+    borderRadius: 8,
+    color: 'white',
+    marginTop: 17,
+  },
 });
+
